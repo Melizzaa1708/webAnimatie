171aa3e8199471217e7be921035e2be9b05127da
Melizzaa1708 <45657066+Melizzaa1708@users.noreply.github.com>
2020-05-14 10:50:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('hellow');
+
+var boot = document.getElementById("Boot");
+var bootani = document.getElementById("Boot");
+
+boot.addEventListener("click", function () {
+    bootani[1].className = "bootani";
+});
