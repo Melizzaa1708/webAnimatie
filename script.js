a38e0d1282439a9907cdbf45b2dd597485a86280
Melizzaa1708 <45657066+Melizzaa1708@users.noreply.github.com>
2020-05-18 12:08:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,62 @@
 console.log('hellow');
 
-var boot = document.getElementById("Boot");
+var lamp = document.getElementById("Lamp");
 
-boot.addEventListener("click", function () {
-    boot.classList.add("bootani");
+lamp.addEventListener("mouseover", function () {
+    document.getElementById("Lichtstraal").style.display = "inline";
 });
+
+lamp.addEventListener("mouseout", function () {
+    document.getElementById("Lichtstraal").style.display = "none";
+});
+
+//
+
+var auto1 = document.getElementById("Auto_1");
+
+auto1.addEventListener("click", function () {
+    document.getElementById("Auto_1").style.animation = "rijden 5s ease";
+    document.getElementById("Auto_2").style.animation = "rijden 5s 0.8s ease";
+    document.getElementById("Auto_3").style.animation = "rijden 5s 0.9s ease";
+});
+
+var auto2 = document.getElementById("Auto_2");
+
+auto2.addEventListener("click", function () {
+    document.getElementById("Auto_1").style.animation = "rijden 5s ease";
+    document.getElementById("Auto_2").style.animation = "rijden 5s 0.8s ease";
+    document.getElementById("Auto_3").style.animation = "rijden 5s 0.9s ease";
+});
+
+var auto3 = document.getElementById("Auto_3");
+
+auto3.addEventListener("click", function () {
+    document.getElementById("Auto_1").style.animation = "rijden 5s ease";
+    document.getElementById("Auto_2").style.animation = "rijden 5s 0.8s ease";
+    document.getElementById("Auto_3").style.animation = "rijden 5s 0.9s ease";
+});
+
+//
+
+var roze = document.getElementById("Roze_vissenkom");
+
+roze.addEventListener("click", function () {
+    document.getElementById("Roze_vissenkom").style.animation = "1s circle reverse";
+});
+
+
+//
+
+var witboot = document.getElementById("Wit_boot");
+
+witboot.addEventListener("mouseover", function () {
+    document.getElementById("Boot").style.animation = "kaboom 4s 0s infinite";
+});
+
+witboot.addEventListener("mouseout", function () {
+    document.getElementById("Boot").style.animation = "0s ease 0s 1 normal none running none";
+});
+
+
+
+
